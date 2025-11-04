fc9685bcd72d983923b1591987248248fc2b10e5
kporceil <163202211+kporceil@users.noreply.github.com>
2025-11-04 22:59:10 +0100

    cpp03 ex02

---
+++ cpp03/ex02/includes/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kporceil <kporceil@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 22:20:37 by kporceil          #+#    #+#             */
+/*   Updated: 2025/11/04 22:52:08 by kporceil         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <string>
+
+class FragTrap: public ClapTrap {
+	public:
+		FragTrap(void);
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap& cpy);
+		~FragTrap(void);
+
+		void	highFivesGuys(void);
+};
+
+#endif
---
+++ cpp03/ex02/srcs/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kporceil <kporceil@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 16:15:58 by kporceil          #+#    #+#             */
+/*   Updated: 2025/11/04 17:11:33 by kporceil         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int	main(void) {
+	ClapTrap	ct1("jean");
+	ClapTrap	ct2("yves");
+	ScavTrap	st1("pierre");
+	ScavTrap	st2("roger");
+	FragTrap	ft1("claude");
+	FragTrap	ft2("lucien");
+
+	ct1.attack("yves");
+	ct2.takeDamage(0);
+	ct2.beRepaired(0);
+	st1.attack("roger");
+	st2.attack("pierre");
+	st1.guardGate();
+	st2.guardGate();
+	ft1.highFivesGuys();
+	ft2.highFivesGuys();
+}
