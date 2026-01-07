41040958f4b0195c38a839f0da2d0b51dda2becd
kporceil <163202211+kporceil@users.noreply.github.com>
2026-01-07 18:05:49 +0100

    cpp07 ex00: added include guard

--- cpp07/ex00/includes/whatever.hpp
+++ cpp07/ex00/includes/whatever.hpp
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
 template <typename T>
 void	swap(T& a, T& b) {
 	T	tmp = a;
@@ -27,3 +30,5 @@ template <typename T>
 T	max(T a, T b) {
 	return (a < b ? b : a);
 }
+
+#endif
