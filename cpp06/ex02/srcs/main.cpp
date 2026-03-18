1b8fceec49e4a4d4a934ba3e7568471278846a87
kporceil <163202211+kporceil@users.noreply.github.com>
2026-03-18 16:09:05 +0100

    fix: remove typeinfo header in cpp06/ex02

--- cpp06/ex02/srcs/main.cpp
+++ cpp06/ex02/srcs/main.cpp
@@ -10,21 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Base.hpp"
-#include "A.hpp"
-#include "B.hpp"
-#include "C.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <exception>
 #include <iostream>
-#include <typeinfo>
+#include "A.hpp"
+#include "B.hpp"
+#include "Base.hpp"
+#include "C.hpp"
 
-Base*	generate(void) {
-	int	rd;
+namespace std
+{
+	class bad_cast : public std::exception
+	{};
+} // namespace std
+
+Base*
+generate(void)
+{
+	int rd;
 
 	srand(clock());
 	rd = rand();
-	switch (rd % 3) {
+	switch (rd % 3)
+	{
 		case 0:
 			std::cout << "Generate A class" << std::endl;
 			return (new A);
@@ -39,43 +48,66 @@ Base*	generate(void) {
 	return (NULL);
 }
 
-void	identify(Base* p) {
-	if (dynamic_cast<A*>(p) != NULL) {
+void
+identify(Base* p)
+{
+	if (dynamic_cast<A*>(p) != NULL)
+	{
 		std::cout << "A" << std::endl;
-	} else if (dynamic_cast<B*>(p) != NULL) {
+	}
+	else if (dynamic_cast<B*>(p) != NULL)
+	{
 		std::cout << "B" << std::endl;
-	} else if (dynamic_cast<C*>(p) != NULL) {
+	}
+	else if (dynamic_cast<C*>(p) != NULL)
+	{
 		std::cout << "C" << std::endl;
-	} else {
+	}
+	else
+	{
 		std::cout << "invalid pointer" << std::endl;
 	}
 }
 
-void	identify(Base& p) {
-	try {
-		(void)dynamic_cast<A&>(p);
+void
+identify(Base& p)
+{
+	try
+	{
+		(void) dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
-	} catch (std::bad_cast& e) {
-		try {
-			(void)dynamic_cast<B&>(p);
+	}
+	catch (std::bad_cast& e)
+	{
+		try
+		{
+			(void) dynamic_cast<B&>(p);
 			std::cout << "B" << std::endl;
-		} catch (std::bad_cast& e) {
-			try {
-				(void)dynamic_cast<C&>(p);
+		}
+		catch (std::bad_cast& e)
+		{
+			try
+			{
+				(void) dynamic_cast<C&>(p);
 				std::cout << "C" << std::endl;
-			} catch (std::bad_cast& e) {
+			}
+			catch (std::bad_cast& e)
+			{
 				std::cout << "invalid reference" << std::endl;
 			}
 		}
 	}
 }
 
-int	main(void) {
-	Base*	test = generate();
+int
+main(void)
+{
+	Base* test = generate();
 
 	std::cout << "Pointer identification: ";
 	identify(test);
 	std::cout << "Reference identification: ";
 	identify(*test);
+	delete test;
 	return (0);
 }
