f20f33af4e90ea8c6edf5d5c9768d9c46753c8e8
kporceil <163202211+kporceil@users.noreply.github.com>
2026-05-17 18:46:35 +0200

    fix(cpp06): add single litteral to scalarConverter

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -20,53 +20,91 @@
 #include <limits>
 #include <string>
 
-ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter()
+{}
 
-ScalarConverter::ScalarConverter(const ScalarConverter& cpy) {(void)cpy;}
+ScalarConverter::ScalarConverter(ScalarConverter const& cpy)
+{
+	(void) cpy;
+}
 
 ScalarConverter::~ScalarConverter() {};
 
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter& cpy) { (void)cpy; return (*this); }
+ScalarConverter&
+ScalarConverter::operator=(ScalarConverter const& cpy)
+{
+	(void) cpy;
+	return (*this);
+}
 
-static int	isInteger(double nb)
+static int
+isInteger(double nb)
 {
 	return (nb == std::floor(nb));
 }
 
-void	ScalarConverter::convert(std::string const& litteral) {
+void
+ScalarConverter::convert(std::string const& litteral)
+{
 	if (litteral.empty())
 	{
 		std::cout << "cannot perform conversion" << std::endl;
-		return ;
+		return;
 	}
-	try {
-		char*	check;
-		double	dnb = std::strtod(litteral.c_str(), &check);
+	try
+	{
+		char*  check;
+		double dnb;
+		if (litteral.size() == 1)
+		{
+			dnb = litteral[0];
+			check = const_cast<char*>(&litteral[1]);
+		}
+		else
+			dnb = std::strtod(litteral.c_str(), &check);
 		if (*check != '\0' && (*check != 'f' || *(check + 1) != '\0'))
 		{
 			std::cout << "cannot perform conversion" << std::endl;
-			return ;
+			return;
 		}
-		if (!std::isnan(dnb) && !std::isinf(dnb) && static_cast<double>(std::numeric_limits<char>::min()) <= dnb && static_cast<double>(std::numeric_limits<char>::max()) >= dnb) {
+		if (!std::isnan(dnb) && !std::isinf(dnb) &&
+			static_cast<double>(std::numeric_limits<char>::min()) <= dnb &&
+			static_cast<double>(std::numeric_limits<char>::max()) >= dnb)
+		{
 			std::cout << "char: ";
 			if (std::isprint(static_cast<char>(dnb)))
 				std::cout << static_cast<char>(dnb);
 			else
 				std::cout << "Non displayable";
 			std::cout << std::endl;
-		} else {
+		}
+		else
+		{
 			std::cout << "char: impossible" << std::endl;
 		}
-		if (!std::isnan(dnb) && !std::isinf(dnb) && static_cast<double>(std::numeric_limits<int>::min()) <= dnb && static_cast<double>(std::numeric_limits<int>::max()) >= dnb) {
+		if (!std::isnan(dnb) && !std::isinf(dnb) &&
+			static_cast<double>(std::numeric_limits<int>::min()) <= dnb &&
+			static_cast<double>(std::numeric_limits<int>::max()) >= dnb)
+		{
 			std::cout << "int: " << static_cast<int>(dnb) << std::endl;
-		} else {
+		}
+		else
+		{
 			std::cout << "int: impossible" << std::endl;
 		}
-		std::cout << "float: " << std::fixed << std::setprecision(isInteger(dnb) && !std::isinf(dnb) ? 1 : 3) << static_cast<float>(dnb) << "f" << std::endl;
-		std::cout << "double: " << std::fixed << std::setprecision(isInteger(dnb) && !std::isinf(dnb) ? 1 : 3) << dnb << std::endl;
-	} catch (std::exception& e) {
-		std::cout << "cannot perform conversion because: " << e.what() << std::endl;
-		return ;
+		std::cout << "float: " << std::fixed
+				  << std::setprecision(isInteger(dnb) && !std::isinf(dnb) ? 1
+																		  : 3)
+				  << static_cast<float>(dnb) << "f" << std::endl;
+		std::cout << "double: " << std::fixed
+				  << std::setprecision(isInteger(dnb) && !std::isinf(dnb) ? 1
+																		  : 3)
+				  << dnb << std::endl;
 	}
-
+	catch (std::exception& e)
+	{
+		std::cout << "cannot perform conversion because: " << e.what()
+				  << std::endl;
+		return;
+	}
 }
